87295945f16f1e0362a89e375ac8f2a3388b29fe
Gustavo G. Pires <32722432+GuhPires@users.noreply.github.com>
2020-05-26 01:00:29 -0300

    Removed unnecessary comments + Finished Serial mode

--- Auto_Stepper/Auto_Stepper.ino
+++ Auto_Stepper/Auto_Stepper.ino
@@ -12,12 +12,12 @@ Stepper xAxisStepper = Stepper(SPR, 2, 4, 3, 5);
 Stepper yAxisStepper = Stepper(SPR, 8, 10, 9, 11);
 Servo dropperServo;
 
-const int maxXSteps = SPR * 3.6;                            // Max steps into X Axis
-const int maxYSteps = SPR * 1.2;                            // Max steps into Y Axis
-const int initRotationsX = maxXSteps / 4.8;                 // Rotations in the x axis needed to get into the first slot
-const int initRotationsY = 0;                               // Rotations in the y axis needed to get into the first slot
+const int maxXSteps = SPR * 3.6;                              // Max steps into X Axis
+const int maxYSteps = SPR * 1.2;                              // Max steps into Y Axis
+const int initRotationsX = maxXSteps / 4.8;                   // Rotations in the x axis needed to get into the first slot
+const int initRotationsY = 0;                                 // Rotations in the y axis needed to get into the first slot
 const int stepsPerSlotX = (maxXSteps - initRotationsX) / 1.9; // Steps between slots in the x axis
-const int stepsPerSlotY = maxYSteps;                        // Steps between slots in the y axis
+const int stepsPerSlotY = maxYSteps;                          // Steps between slots in the y axis
 
 const int colors[2][3] = {{1, 2, 3}, {4, 5, 6}};  // 1 -> White; 2 -> Red; 3 -> Yellow; 4 -> Orange; 5 -> Green; 6 -> Blue;
 
@@ -28,7 +28,7 @@ void setup() {
   yAxisStepper.setSpeed(RPM);
   Serial.begin(9600);
   delay(2000);
-  fullLap();
+//  fullLap();
 }
 
 void loop() {
@@ -51,7 +51,7 @@ void loop() {
     for(int j = 0; j < 3; j++) {
 //      Serial.print("X: ");
 //      Serial.println(j + 1);
-      Serial.println(colors[i][j]);
+//      Serial.println(colors[i][j]);
       if(colors[i][j] == slot) {
         xCoord = j + 1;
         yCoord = i + 1;
@@ -71,7 +71,12 @@ void loop() {
   Serial.println("====================");
   Serial.print("Y ROTATIONS: ");
   Serial.println(yRotations);
-  delay(1000); 
+  delay(1000);
+  xAxisStepper.step(xRotations);
+  yAxisStepper.step(-yRotations);
+  drop(dropperServo);
+  yAxisStepper.step(yRotations);
+  xAxisStepper.step(-xRotations);
   }
 }
 
